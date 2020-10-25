d55751316ed8d6d65c51725be7b17099e42226ad
Jeroen Weener <jweener1996@gmail.com>
2020-10-25 21:57:10 +0100

    Moves search view to actionbar

--- app/src/main/java/com/jwindustries/isitvegan/MainActivity.java
+++ app/src/main/java/com/jwindustries/isitvegan/MainActivity.java
@@ -6,10 +6,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+    private IngredientAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +22,34 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView.LayoutManager layoutManager =
                 new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
         ingredientRecyclerView.setLayoutManager(layoutManager);
-        final IngredientAdapter adapter = new IngredientAdapter(this);
+        adapter = new IngredientAdapter(this);
         ingredientRecyclerView.setAdapter(adapter);
         ingredientRecyclerView.addItemDecoration(new DividerItemDecoration(ingredientRecyclerView.getContext(), DividerItemDecoration.VERTICAL));
+    }
 
-        SearchView searchView = this.findViewById(R.id.search_view);
-        searchView.setOnClickListener(view -> searchView.setIconified(false));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.getMenuInflater().inflate(R.menu.searchbar_menu, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.searchView);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+        menuItem.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem menuItem) {
+                searchView.requestFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem menuItem) {
+                searchView.setQuery("", false);
+                return true;
+            }
+        });
+
+        searchView.setIconifiedByDefault(false);
+        searchView.setIconified(false);
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -38,5 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        return true;
     }
 }
