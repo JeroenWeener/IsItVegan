dd0e7255f9b559c9e60f316b2414314734f44ed1
Jeroen Weener <jweener1996@gmail.com>
2020-10-29 18:53:57 +0100

    Replaces app title with app icon in actionbar

--- app/src/main/java/com/jwindustries/isitvegan/BaseActivity.java
+++ app/src/main/java/com/jwindustries/isitvegan/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.jwindustries.isitvegan;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,5 +10,12 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setIcon(R.drawable.logo);
+            actionBar.setDisplayShowTitleEnabled(false);
+        }
     }
 }
