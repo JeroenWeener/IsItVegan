4b328d8bb54f8f7a3d32023ca858083b8ea85b52
Jeroen Weener <jweener1996@gmail.com>
2021-04-29 02:07:24 +0200

    Implements app sharing feature

--- app/src/main/java/com/jwindustries/isitvegan/Activities/AboutActivity.java
+++ app/src/main/java/com/jwindustries/isitvegan/Activities/AboutActivity.java
@@ -1,7 +1,10 @@
 package com.jwindustries.isitvegan.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.jwindustries.isitvegan.BuildConfig;
 import com.jwindustries.isitvegan.R;
 
 public class AboutActivity extends BaseActivity {
@@ -10,5 +13,19 @@ public class AboutActivity extends BaseActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public void shareApp(View view) {
+        try {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            String shareMessage = this.getString(R.string.share_message) + "\n\n" +
+                    this.getString(R.string.app_name) +
+                    "\nhttps://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID;
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            startActivity(shareIntent);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
