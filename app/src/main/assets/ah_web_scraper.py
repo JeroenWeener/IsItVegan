ccd6bc1e35156b7896ad9d464408d0754fffc177
Jeroen Weener <jweener1996@gmail.com>
2022-03-19 11:38:18 +0100

    Improve AH scraper; add ability to scan for all products

--- app/src/main/assets/ah_web_scraper.py
+++ app/src/main/assets/ah_web_scraper.py
@@ -1,29 +1,45 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-url = "https://www.ah.nl/producten/product/wi236486"
-req = requests.get(url)
-soup = BeautifulSoup(req.text, "html.parser")
+def main():
+    baseurl = 'https://www.ah.nl/producten/product/wi'
+    all_ingredients = []
 
-ingredients_label = soup.find_all("h2", text="Ingrediënten")[0]
-ingredients_div = ingredients_label.find_parent("div")
-ingredient_text = ingredients_div.find_all("span")[0].text
-ingredients = ingredient_text\
-    .replace("Ingrediënten: ", "")\
-    .replace("(", ', ')\
-    .replace(')', '')\
-    .replace('%', '')\
-    .replace('0', '')\
-    .replace('1', '')\
-    .replace('2', '')\
-    .replace('3', '')\
-    .replace('4', '')\
-    .replace('5', '')\
-    .replace('6', '')\
-    .replace('7', '')\
-    .replace('8', '')\
-    .replace('9', '')\
-    .split(', ')
+    for i in range(507377, 507387):
+        url = baseurl + str(i)
+        ingredients = parseUrl(url)
+        all_ingredients.extend(ingredients)
 
-for ingredient in ingredients:
-    print(ingredient)
+    print(all_ingredients)
+
+
+def parseUrl(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    ingredient_labels = soup.find_all('h2', text='Ingrediënten')
+    if len(ingredient_labels) == 0:
+        return []
+    ingredients_div = ingredient_labels[0].find_parent('div')
+    ingredient_text = ingredients_div.find_all('span')[0].text
+    ingredient_text = re.sub(r'[0-9]+(,[0-9]+)?%', '', ingredient_text).split('.')[0]
+    ingredients = ingredient_text\
+        .replace('Ingrediënten: ', '')\
+        .replace('(', ',')\
+        .replace(')', '')\
+        .replace('[', ',')\
+        .replace(']', '')\
+        .replace('*', '')\
+        .replace('°', '')\
+        .replace('ᵇ', '')\
+        .lower()\
+        .split(',')
+
+    ingredients = [ingredient.replace(' ,', '').replace('bevatten ', '').replace('conserveermiddel: ', '').replace('voedingszuur ', '').replace('bevat: ', '').replace('van ', '').strip() for ingredient in ingredients]
+    ingredients = [ingredient for ingredient in ingredients if len(ingredient) > 0]
+    return ingredients
+
+
+if __name__ == "__main__":
+    main()
