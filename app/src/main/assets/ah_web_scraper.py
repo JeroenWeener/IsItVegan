cce391ef4127910de8d84c9412131e5674673e66
Jeroen Weener <jweener1996@gmail.com>
2022-05-16 14:01:49 +0200

    Updates AH scraper

--- app/src/main/assets/ah_web_scraper.py
+++ app/src/main/assets/ah_web_scraper.py
@@ -2,19 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+
 def main():
     baseurl = 'https://www.ah.nl/producten/product/wi'
     all_ingredients = []
 
     for i in range(507377, 507387):
         url = baseurl + str(i)
-        ingredients = parseUrl(url)
+        ingredients = parse_url(url)
         all_ingredients.extend(ingredients)
 
     print(all_ingredients)
 
 
-def parseUrl(url):
+def parse_url(url):
     req = requests.get(url)
     soup = BeautifulSoup(req.text, 'html.parser')
 
@@ -24,21 +25,32 @@ def parseUrl(url):
     ingredients_div = ingredient_labels[0].find_parent('div')
     ingredient_text = ingredients_div.find_all('span')[0].text
     ingredient_text = re.sub(r'[0-9]+(,[0-9]+)?%', '', ingredient_text).split('.')[0]
-    ingredients = ingredient_text\
-        .replace('Ingrediënten: ', '')\
-        .replace('(', ',')\
-        .replace(')', '')\
-        .replace('[', ',')\
-        .replace(']', '')\
-        .replace('*', '')\
-        .replace('°', '')\
-        .replace('ᵇ', '')\
-        .lower()\
+    ingredients = ingredient_text \
+        .replace('Ingrediënten: ', '') \
+        .replace('(', ',') \
+        .replace(')', '') \
+        .replace('[', ',') \
+        .replace(']', '') \
+        .replace('*', '') \
+        .replace('°', '') \
+        .replace('ᵇ', '') \
+        .lower() \
         .split(',')
 
-    ingredients = [ingredient.replace(' ,', '').replace('bevatten ', '').replace('conserveermiddel: ', '').replace('voedingszuur ', '').replace('bevat: ', '').replace('van ', '').strip() for ingredient in ingredients]
+    ingredients = [
+        ingredient
+            .replace(' ,', '')
+            .replace('bevatten ', '')
+            .replace('conserveermiddel: ', '')
+            .replace('voedingszuur ', '')
+            .replace('verdikkingsmiddel: ', '')
+            .replace('emulgator: ', '')
+            .replace('vitamines: ', '')
+            .replace('bevat: ', '')
+            .replace('onze belangrijkste bestanddelen zijn: ', '')
+            .strip() for ingredient in ingredients]
     ingredients = [ingredient for ingredient in ingredients if len(ingredient) > 0]
-    return ingredients
+    return list(set(ingredients))
 
 
 if __name__ == "__main__":
